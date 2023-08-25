70907851d59b459c9921e3af312276aaa180f0e3
Skelon_Chan <269529898@msn.cn>
2023-08-25 15:13:42 +0800

    none: 仅用于同步代码

--- test_cameras.py
+++ test_cameras.py
@@ -3,6 +3,7 @@
 import cv2
 import time
 
+
 # 自动搜索获取安装的相机列表用于初始化
 def get_cam_lst(cam_lst=range(0, 24)):
     arr = []
@@ -16,52 +17,60 @@ def get_cam_lst(cam_lst=range(0, 24)):
         cap.release()
     return arr
 
+
 def show_cam_img(caps, cam_list):
-    print("INFO: Press 'q' to quit! Press 's' to save a picture, 'n' to change to next camera device!")
+    print(
+        "INFO: Press 'q' to quit! Press 's' to save a picture, 'n' to change to next camera device!"
+    )
     idx = 0
     while True:
         cap_device = caps[idx]
         ret, frame = cap_device.read()
         if ret:
-            cv2.imshow('video', frame)
+            cv2.imshow("video", frame)
         else:
             print("ERROR: failed read frame!")
 
         # quit the test
         c = cv2.waitKey(1)
-        if c == ord('q'):
+        if c == ord("q"):
             break
 
         # change to next camera device
-        if c == ord('n'):
+        if c == ord("n"):
             idx += 1
             if idx >= len(caps):
                 idx = 0
             continue
 
         # save the picture
-        if c == ord('s'):
+        if c == ord("s"):
             if ret:
-                name = 'video{0}_{1}.png'.format(cam_list[idx],
-                            time.strftime("%Y-%m-%d_%H:%M:%S", time.localtime()))
-                cv2.imwrite("E:\\front.png", frame) #保存可能有问题，自己指定路径和文件名
-                print("saved file: %s!" %name)
+                name = "video{0}_{1}.png".format(
+                    cam_list[idx], time.strftime("%Y-%m-%d_%H:%M:%S", time.localtime())
+                )
+                cv2.imwrite("E:\\front.png", frame)  # 保存可能有问题，自己指定路径和文件名
+                print("saved file: %s!" % name)
 
     cv2.destroyAllWindows()
 
-def init_caps(cam_list, resolution=(640,480)):
+
+def init_caps(cam_list, resolution=(640, 480)):
     caps = []
     for iCam in cam_list:
         cap = cv2.VideoCapture(iCam)
+        cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*"MJPG"))
         cap.set(3, resolution[0])
         cap.set(4, resolution[1])
         caps.append(cap)
 
     return caps
 
+
 def deinit_caps(cap_list):
     for cap in cap_list:
         cap.release()
+
 
 def show_cameras(video_list=None):
     if video_list == None:
@@ -70,8 +79,10 @@ def show_cameras(video_list=None):
         err_msg = "cannot find any video device!"
     else:
         cam_list = get_cam_lst(video_list)
-        err_msg = "cannot find available video device in list: {0}!".format(video_list) +\
-                    "\nPlease check the video devices in /dev/v4l/by-path/ folder!"
+        err_msg = (
+            "cannot find available video device in list: {0}!".format(video_list)
+            + "\nPlease check the video devices in /dev/v4l/by-path/ folder!"
+        )
 
     if len(cam_list) < 1:
         print("ERROR: " + err_msg)
@@ -82,9 +93,10 @@ def show_cameras(video_list=None):
     show_cam_img(caps, cam_list)
     deinit_caps(caps)
 
+
 if __name__ == "__main__":
     # 可以使用如下指令指定打开的相机号
-    #show_cameras([2, 6, 10, 14])
+    # show_cameras([2, 6, 10, 14])
 
     # 也可以自动搜索全部的相机,`q`退出，`s`保存一张图片，`n`切换下张照片
     show_cameras()
